d5cd703a880ea60fa2e588ebafbb1488dacbc96c
Nicole Beguesse <nicole@highlinesale.com>
2013-06-07 16:26:15 -0400

    Oops

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,10 @@
 class MainController < ApplicationController
-  before_filter: set_access_control_headers
+  before_filter :set_access_control_headers
 def  set_access_control_headers
-          headers['Access-Control-Allow-Origin'] = ‘*’
-          headers['Access-Control-Request-Method'] = ‘*’
-          headers['Access-Control-Allow-Headers'] = ‘*’
-          headers['Access-Control-Allow-Credentials'] = “true”
+          headers['Access-Control-Allow-Origin'] = '*'
+          headers['Access-Control-Request-Method'] = '*'
+          headers['Access-Control-Allow-Headers'] = '*'
+          headers['Access-Control-Allow-Credentials'] = "true"
   end
 
   def cookie
